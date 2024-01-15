4941a04d12c32a74bd961ff83f7a15dc21282cad
pasha <askerovpasha21@gmail.com>
2024-01-15 15:56:00 +0400

    mobile menu completed

--- js/script.js
+++ js/script.js
@@ -6,8 +6,24 @@ document.addEventListener('DOMContentLoaded', function () {
         });
 
         //mobile search
-        $('.nav-tool.mobile-search').click(function(){
+        $('.nav-tool.mobile-search-button').click(function(){
             $('.search-form.mobile').toggleClass('active');
+        });
+
+        // mobile menu
+        $('.nav-tool.mobile-menu-button').click(function(){
+            $('.black-backdrop').show();
+            $('.mobile-menu').addClass('active')
+        });
+        $('.nav-tool.menu-close-button').click(function(){
+            $('.black-backdrop').hide();
+            $('.mobile-menu').removeClass('active')
+        })
+
+        // category
+        $('.category.has-child .toggle-button').click(function(){
+            let id = $(this).closest('.category.has-child').data('id');
+            $(`.altcategories[data-parent="${id}"]`).toggle();
         })
     });
 });
