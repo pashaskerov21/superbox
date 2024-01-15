1de24a1e927a3bb783b66a3b7518e56ed3a5a731
pasha <askerovpasha21@gmail.com>
2024-01-15 12:39:47 +0400

    mobile general nav completed

---
+++ js/script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function () {
+    $(document).ready(function(){
+        // top alert button click
+        $('.top-alert-wrapper .close-button').click(function(){
+            $('.top-alert-wrapper').hide();
+        });
+    });
+});
