025dfb300ec2a72eb822a8a87d9b465b8181c779
pasha <askerovpasha21@gmail.com>
2024-01-16 10:54:02 +0400

    product card completed

--- js/script.js
+++ js/script.js
@@ -26,8 +26,41 @@ document.addEventListener('DOMContentLoaded', function () {
 
         // category
         $('.category.has-child .toggle-button').click(function(){
-            let id = $(this).closest('.category.has-child').data('id');
-            $(`.altcategories[data-parent="${id}"]`).toggle();
-        })
+            if($(window).width() < 992){
+                let id = $(this).closest('.category.has-child').data('id');
+                $(`.altcategories[data-parent="${id}"]`).toggle();
+            }
+        });
+
+        $('.category.has-child').hover(function(){
+            if($(window).width() > 992){
+                let id = $(this).data('id');
+                $(`.altcategories[data-parent="${id}"]`).show();
+            }
+        }, function(){
+            if($(window).width() > 992){
+                let id = $(this).data('id');
+                $(`.altcategories[data-parent="${id}"]`).hide();
+            }
+        });
+
+        // product
+        $('.favorite-button').click(function(){
+            if($(this).find('i').hasClass('fa-regular')){
+                $(this).find('i').removeClass('fa-regular');
+                $(this).find('i').addClass('fa-solid');
+            }else{
+                $(this).find('i').removeClass('fa-solid');
+                $(this).find('i').addClass('fa-regular');
+            }
+        });
+
+        function setProductCartHeight() {
+            let cartWidth = $('.product-card .product-image').innerWidth();
+            $('.product-card .product-image').css('height', cartWidth + 'px');
+        }
+        $(window).on('load resize', function() {
+            setProductCartHeight();
+        });
     });
 });
--- js/swiper.js
+++ js/swiper.js
@@ -1,5 +1,169 @@
 document.addEventListener('DOMContentLoaded', function () {
-    $(document).ready(function(){
+    $(document).ready(function () {
+        const bannerSwiper = new Swiper('.banner-swiper', {
+            loop: true,
+            spaceBetween: 20,
+            autoplay: {
+                delay: 5000,
+                disableOnInteraction: false,
+            },
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+                dynamicMainBullets: 5,
+            },
+            navigation: {
+                prevEl: '.swiper-button-prev',
+                nextEl: '.swiper-button-next',
+            },
+        });
 
+        const productSwiper_1 = new Swiper('.product-swiper-1', {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+                dynamicMainBullets: 6,
+            },
+            breakpoints: {
+                576: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                    spaceBetween: 20,
+                },
+                768:{
+                    slidesPerView: 3,
+                    slidesPerGroup: 3,
+                    spaceBetween: 20,
+                },
+                992:{
+                    slidesPerView: 3,
+                    slidesPerGroup: 3,
+                    spaceBetween: 80,
+                },
+                1200:{
+                    slidesPerView: 4,
+                    slidesPerGroup: 4,
+                    spaceBetween: 20,
+                },
+                1400:{
+                    slidesPerView: 5,
+                    slidesPerGroup: 5,
+                    spaceBetween: 30,
+                },
+
+            }
+        });
+        const productSwiper_2 = new Swiper('.product-swiper-2', {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+                dynamicMainBullets: 6,
+            },
+            breakpoints: {
+                576: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                    spaceBetween: 20,
+                },
+                768:{
+                    slidesPerView: 3,
+                    slidesPerGroup: 3,
+                    spaceBetween: 20,
+                    grid: {
+                        fill: 'row',
+                        rows: 2,
+                    },
+                },
+                992:{
+                    slidesPerView: 3,
+                    slidesPerGroup: 3,
+                    spaceBetween: 80,
+                    grid: {
+                        fill: 'row',
+                        rows: 2,
+                    },
+                },
+                1200:{
+                    slidesPerView: 4,
+                    slidesPerGroup: 4,
+                    spaceBetween: 20,
+                    grid: {
+                        fill: 'row',
+                        rows: 2,
+                    },
+                },
+                1400:{
+                    slidesPerView: 5,
+                    slidesPerGroup: 5,
+                    spaceBetween: 30,
+                    grid: {
+                        fill: 'row',
+                        rows: 2,
+                    },
+                },
+
+            }
+        })
+        const productSwiper_3 = new Swiper('.product-swiper-3', {
+            slidesPerView: 1,
+            spaceBetween: 20,
+            pagination: {
+                el: ".swiper-pagination",
+                clickable: true,
+                dynamicBullets: true,
+                dynamicMainBullets: 6,
+            },
+            breakpoints: {
+                576: {
+                    slidesPerView: 2,
+                    slidesPerGroup: 2,
+                    spaceBetween: 20,
+                },
+                768:{
+                    slidesPerView: 3,
+                    slidesPerGroup: 3,
+                    spaceBetween: 20,
+                    grid: {
+                        fill: 'row',
+                        rows: 2,
+                    },
+                },
+                992:{
+                    slidesPerView: 3,
+                    slidesPerGroup: 3,
+                    spaceBetween: 80,
+                    grid: {
+                        fill: 'row',
+                        rows: 2,
+                    },
+                },
+                1200:{
+                    slidesPerView: 4,
+                    slidesPerGroup: 4,
+                    spaceBetween: 20,
+                    grid: {
+                        fill: 'row',
+                        rows: 3,
+                    },
+                },
+                1400:{
+                    slidesPerView: 5,
+                    slidesPerGroup: 5,
+                    spaceBetween: 30,
+                    grid: {
+                        fill: 'row',
+                        rows: 3,
+                    },
+                },
+
+            }
+        });
     });
 });
